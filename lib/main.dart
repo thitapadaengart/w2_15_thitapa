5020dd876d6b1e747892a318da7581622e135c04
thitapadaengart <thitapa.daengart@e-tech.ac.th>
2026-08-05 15:17:27 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      // แก้ไข Syntax error จาก .fromSeed เป็น ColorScheme.fromSeed
       theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
